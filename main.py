b76be2b4838435f4b619ac43ceb7334231d79fef
Germanized <91925088+Germanized@users.noreply.github.com>
2024-08-30 19:35:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ class FaceBook_RegIster():
             "ccp": "",
             "reg_instance": "",
             "submission_request": "",
-            "reg_impression_id": ""
+            "reg_impression_id": "",
+            "__s": "",
+            "__hsi": ""
         }
         self.password = "".join(random.choice("1234567890qpwoeirutyalskdjfhgmznxbcv") for _ in range(10))
         self.email = "whisper" + "".join(random.choice("1234567890qpwoeirutyalskdjfhgmznxbcv") for _ in range(15))
@@ -29,18 +31,17 @@ class FaceBook_RegIster():
         url = "https://mbasic.facebook.com/reg/?cid=103&refsrc=deprecated&_rdr"
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'html.parser')
-        lsd = soup.select_one('input[name=lsd]')['value']
-        jazoest = soup.select_one('input[name=jazoest]')['value']
-        ccp = soup.select_one('input[name=ccp]')['value']
-        reg_instance = soup.select_one('input[name=reg_instance]')['value']
-        submission_request = soup.select_one('input[name=submission_request]')['value']
-        reg_impression_id = soup.select_one('input[name=reg_impression_id]')['value']
-        self.cookies['lsd'] = lsd
-        self.cookies['jazoest'] = jazoest
-        self.cookies['ccp'] = ccp
-        self.cookies['reg_instance'] = reg_instance
-        self.cookies['submission_request'] = submission_request
-        self.cookies['reg_impression_id'] = reg_impression_id
+        self.cookies['lsd'] = soup.select_one('input[name=lsd]')['value']
+        self.cookies['jazoest'] = soup.select_one('input[name=jazoest]')['value']
+        self.cookies['ccp'] = soup.select_one('input[name=ccp]')['value']
+        self.cookies['reg_instance'] = soup.select_one('input[name=reg_instance]')['value']
+        self.cookies['submission_request'] = soup.select_one('input[name=submission_request]')['value']
+        self.cookies['reg_impression_id'] = soup.select_one('input[name=reg_impression_id]')['value']
+        try:
+            self.cookies['__s'] = soup.select_one('input[name=__s]')['value']
+            self.cookies['__hsi'] = soup.select_one('input[name=__hsi]')['value']
+        except TypeError:
+            print("Couldn't find __s or __hsi values")
 
     def save_to_text_file(self, username, email, password, status):
         with open('account_info.txt', 'a') as file:
@@ -70,7 +71,18 @@ class FaceBook_RegIster():
             "Sec-Fetch-User": "?1",
             "Te": "trailers"
         }
-        data = f"lsd={self.cookies['lsd']}&jazoest={self.cookies['jazoest']}&ccp={self.cookies['ccp']}&reg_instance={self.cookies['reg_instance']}&submission_request={self.cookies['submission_request']}&helper=&reg_impression_id={self.cookies['reg_impression_id']}&ns=0&zero_header_af_client=&app_id=&logger_id=&field_names%5B%5D=firstname&field_names%5B%5D=reg_email__&field_names%5B%5D=sex&field_names%5B%5D=birthday_wrapper&field_names%5B%5D=reg_passwd__&firstname={self.Name}&lastname={self.Name2}&reg_email__={self.email}%40yopmail.com&sex={random.randint(1,2)}&custom_gender=&did_use_age=false&birthday_month={random.randint(1,12)}&birthday_day={random.randint(1,28)}&birthday_year={random.randint(1996,2005)}&age_step_input=&reg_passwd__={self.password}&submit=Sign+Up"
+        data = (
+            f"lsd={self.cookies['lsd']}&jazoest={self.cookies['jazoest']}&ccp={self.cookies['ccp']}&reg_instance="
+            f"{self.cookies['reg_instance']}&submission_request={self.cookies['submission_request']}&helper=&reg_impression_id="
+            f"{self.cookies['reg_impression_id']}&ns=0&zero_header_af_client=&app_id=&logger_id=&field_names%5B%5D=firstname&field_names%5B%5D=reg_email__&field_names%5B%5D=sex&field_names%5B%5D=birthday_wrapper&field_names%5B%5D=reg_passwd__&firstname="
+            f"{self.Name}&lastname={self.Name2}&reg_email__={self.email}%40yopmail.com&sex={random.randint(1,2)}&custom_gender=&did_use_age=false&birthday_month="
+            f"{random.randint(1,12)}&birthday_day={random.randint(1,28)}&birthday_year={random.randint(1996,2005)}&age_step_input=&reg_passwd__={self.password}&submit=Sign+Up"
+        )
+        if self.cookies['__s']:
+            data += f"&__s={self.cookies['__s']}"
+        if self.cookies['__hsi']:
+            data += f"&__hsi={self.cookies['__hsi']}"
+
         r = requests.post(url, headers=headers, data=data)
         if 'take you through a few steps to confirm your account on Facebook' in r.text:
             print('[+] Done Create Account !')
